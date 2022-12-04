7eabc6616eba3ab37c886c57103a2abe52b0a089
MaximilienH <maximilienherr@gmail.com>
2022-12-04 19:01:40 +0100

    Ajout DashboardPanel.vue component from Dashboard.vue page

--- Frontend/FrontEndWithVite/src/pages/Dashboard.vue
+++ Frontend/FrontEndWithVite/src/pages/Dashboard.vue
@@ -1,15 +1,18 @@
 <template>
 	<div id="app">
 		<Header />
+        <DashboardAdmin />
 	</div>
 </template>
 
 <script>
 import Header from '../components/Header.vue';
+import DashboardAdmin from '../components/DashboardPanel.vue';
 export default {
 	name: 'app',
 	components: {
-		Header
+		Header,
+        DashboardAdmin
 	}
 }
 </script>
