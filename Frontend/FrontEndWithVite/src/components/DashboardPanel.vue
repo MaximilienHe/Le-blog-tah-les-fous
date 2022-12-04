795fa033bdfdebb8d07ae4d728b7b4e511df4b33
MaximilienH <maximilienherr@gmail.com>
2022-12-04 19:02:50 +0100

    Ajout DashboardPanel.vue

---
+++ Frontend/FrontEndWithVite/src/components/DashboardPanel.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="dashboard">
+        <img src="../assets/S22ultra.jpg" />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'admin-dashboard'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
