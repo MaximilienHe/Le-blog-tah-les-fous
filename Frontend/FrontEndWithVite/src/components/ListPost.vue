40c7608c96397b83ea6f0b091a2514b28e0cd327
MaximilienH <maximilienherr@gmail.com>
2022-12-05 09:59:59 +0100

    Beginning list comments

--- Frontend/FrontEndWithVite/src/components/ListPost.vue
+++ Frontend/FrontEndWithVite/src/components/ListPost.vue
@@ -1,124 +1,131 @@
 <template>
-	<section class="latest">
-		<h3>Derniers articles</h3>
-		<div class="posts">
-			<div class="post">
-				<div class="image" :style="{ backgroundImage: `url('')` }"></div>
-				<h4>Plan Ahead</h4>
-			</div>
-			<div class="post col-2 row-2">
-				<div class="image" :style="{ backgroundImage: `url('')` }"></div>
-				<h4>VueJS Fundamentals</h4>
-			</div>
-			<div class="post row-2">
-				<div class="image" :style="{ backgroundImage: `url('')}')` }"></div>
-				<h4>Everything wrong with AI</h4>
-			</div>
-			<div class="post row-2">
-				<div class="image" :style="{ backgroundImage: `url('')}')` }"></div>
-				<h4>#100DaysOfCode</h4>
-			</div>
-			<div class="post">
-				<div class="image" :style="{ backgroundImage: `url('')}')` }"></div>
-				<h4>Node Package Manager</h4>
-			</div>
-			<div class="post col-2">
-				<div class="image" :style="{ backgroundImage: `url('')}')` }"></div>
-				<h4>Learn to take a break</h4>
-			</div>
-		</div>
+    <section class="latest">
+        <h3>Derniers articles</h3>
+        <div class="posts">
+            <div class="post">
+                <div class="image" :style="{ backgroundImage: `url('')` }"></div>
+                <h4>Plan Ahead</h4>
+            </div>
+            <div class="post col-2 row-2">
+                <div class="image" :style="{ backgroundImage: `url('')` }"></div>
+                <h4>VueJS Fundamentals</h4>
+            </div>
+            <div class="post row-2">
+                <div class="image" :style="{ backgroundImage: `url('')}')` }"></div>
+                <h4>Everything wrong with AI</h4>
+            </div>
+            <div class="post row-2">
+                <div class="image" :style="{ backgroundImage: `url('')}')` }"></div>
+                <h4>#100DaysOfCode</h4>
+            </div>
+            <div class="post">
+                <div class="image" :style="{ backgroundImage: `url('')}')` }"></div>
+                <h4>Node Package Manager</h4>
+            </div>
+            <div class="post col-2">
+                <div class="image" :style="{ backgroundImage: `url('')}')` }"></div>
+                <h4>Learn to take a break</h4>
+            </div>
+        </div>
 
-		<div v-for="post in posts" class="listPost">
-			<PostItem class="item" :post="post" textButton="Lire l'article" :handleClick="this.readArticle" />
-		</div>
-	</section>
+        <div v-for="post in posts" class="listPost">
+            <PostItem class="item" :post="post" textButton="Lire l'article" :handleClick="this.readArticle" />
+        </div>
+    </section>
 </template>
 
 <script>
 import { usePiniaStore } from '../stores/postsStore';
 import PostItem from './PostItem.vue';
 export default {
-	props: ['msg'],
-	name: 'latest-articles',
-	components: {
-		PostItem
-	},
-	// Properties returned fsrom data() become reactive state
-	// and will be exposed on `this`.
-	data() {
-		return {
-			posts: [],
-		};
-	},
-	// Methods are functions that mutate state and trigger updates.
-	// They can be bound as event listeners in templates.
-	methods: {
-		readArticle(data) {
-			this.$router.push({
-				name: 'Article',
-				params: { id: data.id },
-			});
-		},
-	},
-	// Lifecycle hooks are called at different stages
-	// of a component's lifecycle.
-	// This function will be called when the component is mounted.
-	mounted() {
-		const posts = usePiniaStore();
-		fetch('../src/assets/articles.json')
-			.then((res) => res.json())
-			.then((res) => {
-				this.posts = res.data;
-				posts.setPosts(res.data);
-			});
-	},
+    props: ['msg'],
+    name: 'latest-articles',
+    components: {
+        PostItem
+    },
+    // Properties returned fsrom data() become reactive state
+    // and will be exposed on `this`.
+    data() {
+        return {
+            posts: [],
+        };
+    },
+    // Methods are functions that mutate state and trigger updates.
+    // They can be bound as event listeners in templates.
+    methods: {
+        readArticle(data) {
+            this.$router.push({
+                name: 'Article',
+                params: { id: data.id },
+            });
+        },
+    },
+    // Lifecycle hooks are called at different stages
+    // of a component's lifecycle.
+    // This function will be called when the component is mounted.
+    mounted() {
+        const posts = usePiniaStore();
+        fetch('../src/assets/articles.json')
+            .then((res) => res.json())
+            .then((res) => {
+                this.posts = res.data;
+                posts.setPosts(res.data);
+            });
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 .item {
-	margin-bottom: 50px;
+    margin-bottom: 50px;
 }
-.listPost {
-}
+
+.listPost {}
+
 .latest {
-	display: block;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	.posts {
-		display: grid;
-		grid-template-columns: repeat(4, 1fr);
-		grid-template-rows: repeat(3, 196px);
-		grid-gap: 30px;
-		margin: 15px;
-		.post {
-			display: flex;
-			flex-direction: column;
-			background-color: #FFFFFF;
-			box-shadow: 3px 3px 8px 0px rgba(0, 0, 0, 0.2);
-			.image {
-				flex: 1;
-				background-position: center;
-				background-size: cover;
-				background-repeat: no-repeat;
-			}
-			h4 {
-				box-sizing: border-box;
-				padding: 3px 15px 0px 15px;
-				height: 50px;
-				line-height: 50px;
-				margin: 0px;
-			}
-			&.col-2 {
-				grid-column: span 2;
-			}
-			&.row-2 {
-				grid-row: span 2;
-			}
-		}
-	}
+    display: block;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+
+    .posts {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(3, 196px);
+        grid-gap: 30px;
+        margin: 15px;
+
+        .post {
+            display: flex;
+            flex-direction: column;
+            background-color: #FFFFFF;
+            box-shadow: 3px 3px 8px 0px rgba(0, 0, 0, 0.2);
+
+            .image {
+                flex: 1;
+                background-position: center;
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+
+            h4 {
+                box-sizing: border-box;
+                padding: 3px 15px 0px 15px;
+                height: 50px;
+                line-height: 50px;
+                margin: 0px;
+            }
+
+            &.col-2 {
+                grid-column: span 2;
+            }
+
+            &.row-2 {
+                grid-row: span 2;
+            }
+        }
+    }
 }
 </style>
--- Frontend/FrontEndWithVite/src/components/PostDetail.vue
+++ Frontend/FrontEndWithVite/src/components/PostDetail.vue
@@ -1,227 +1,230 @@
 <script>
 import { usePiniaStore } from "../stores/postsStore";
 import Header from "../components/Header.vue";
+import CommentItem from "../components/CommentItem.vue";
+
 export default {
-  props: ["id", "postItem"],
-  components: {
-    Header
-  },
-  // Properties returned fsrom data() become reactive state
-  // and will be exposed on `this`.
-  data() {
-    return {
-      post: undefined,
-    };
-  },
-  // Methods are functions that mutate state and trigger updates.
-  // They can be bound as event listeners in templates.
-  methods: {},
-  // Lifecycle hooks are called at different stages
-  // of a component's lifecycle.
-  // This function will be called when the component is mounted.
-  mounted() {
-    const postStore = usePiniaStore();
-    this.post = postStore.getPost(this.$route.params.id);
-    console.log(this.post);
-  }
+	props: ["id", "postItem"],
+	components: {
+		Header,
+		CommentItem
+	},
+	// Properties returned fsrom data() become reactive state
+	// and will be exposed on `this`.
+	data() {
+		return {
+			post: undefined,
+			comments : []
+		};
+	},
+	// Methods are functions that mutate state and trigger updates.
+	// They can be bound as event listeners in templates.
+	methods: {},
+	// Lifecycle hooks are called at different stages
+	// of a component's lifecycle.
+	// This function will be called when the component is mounted.
+	mounted() {
+		const postStore = usePiniaStore();
+		this.post = postStore.getPost(this.$route.params.id);
+		console.log(this.post);
+
+		let URL = '/articles/' + this.post.id + '/comments';
+		fetch(URL)
+            .then((res) => res.json())
+            .then((res) => {
+                this.comments = res.data;
+            });
+	}
 };
 </script>
 
 <template>
 
-  <Header />
+	<Header />
 
-  <body>
-    <div class="banner">
-      <img v-if="post" :src="post.img" />
-    </div>
-    <div class="direction">
-      <h1 v-if="post">{{ post.title }}</h1>
-      <div id="infos">
-        <div v-if="post" class="author">{{ post.author }}</div>
-        <div v-if="post" class="date">{{ post.createdAt }}</div>
-      </div>
-    </div>
-    <div class="fix">
-      <button class="favorite"><img src="../assets/bookmark.jpg" id="fixedbutton"></button>
-    </div>
+	<body>
+		<div class="banner">
+			<img v-if="post" :src="post.img" />
+		</div>
+		<div class="direction">
+			<h1 v-if="post">{{ post.title }}</h1>
+			<div id="infos">
+				<div v-if="post" class="author">{{ post.author }}</div>
+				<div v-if="post" class="date">{{ post.createdAt }}</div>
+			</div>
+		</div>
+		<div class="fix">
+			<button class="favorite"><img src="../assets/bookmark.jpg" id="fixedbutton"></button>
+		</div>
 
-    <div class="content">
-      <p>
-        200 mégapixels, 120 Watts … Ce Xiaomi 12T Pro affiche une fiche technique
-        avec de « gros » chiffres. Sur le papier, cela devrait annoncer de
-        meilleures caractéristiques. On sait dès notre plus tendre enfance qu’il
-        vaut mieux 2 cookies qu’un seul. Mais la tech n’a pas besoin de ces « gros
-        » chiffres, et le Xiaomi 12T Pro le montre bien.
-      </p>
+		<div class="content"> {{ post.content }}</div>
 
-      <h2>Notre avis en bref sur le Xiaomi 12T Pro</h2>
-      <h3>Unboxing du Xiaomi 12T Pro</h3>
-
-      <!-- <div id="IMGarticle">
+		<!-- <div id="IMGarticle">
         <img id="articleIMG" src="https://droidsoft.fr/wp-content/uploads/2022/11/Xiaomi-12T-Pro-miniature-scaled.jpg"
           data-v-cf937a3e />
         <div id="gradiant"></div>
       </div> -->
-    </div>
-  </body>
+		<div v-for="comment in comments" class="listComments">
+			<CommentItem class="item" :comment="comment" />
+		</div>
+	</body>
 </template>
 
 <style scoped>
 .content {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
-  padding-left: 20vh;
-  padding-right: 20vh;
-  color: white;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: left;
+	padding-left: 20vh;
+	padding-right: 20vh;
+	color: white;
 }
 
 
 .banner {
-  position: relative;
-  padding-left: 7.5vw;
+	position: relative;
+	padding-left: 7.5vw;
 
 }
 
 .img {
-  width: 100%;
-  height: auto;
-  opacity: 0.6;
-  color: white;
-  border-radius: 20px;
+	width: 100%;
+	height: auto;
+	opacity: 0.6;
+	color: white;
+	border-radius: 20px;
 }
 
 #infos {
-  display: flex;
-  flex-direction: row;
+	display: flex;
+	flex-direction: row;
 }
 
 
 h1 {
-  font-size: 78px;
-  margin-left: 50px;
-  margin-right: 50px;
-  color: white;
+	font-size: 78px;
+	margin-left: 50px;
+	margin-right: 50px;
+	color: white;
 }
 
 .author {
-  margin-left: 50px;
-  margin-right: 50px;
+	margin-left: 50px;
+	margin-right: 50px;
 }
 
 .date {
-  margin-left: 50px;
-  margin-right: 50px;
+	margin-left: 50px;
+	margin-right: 50px;
 }
 
 .direction {
-  position: absolute;
-  bottom: -12.7vh;
-  left: 8.40vw;
-  color: rgb(255, 255, 255);
-  background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
-  width: 1600px;
+	position: absolute;
+	bottom: -12.7vh;
+	left: 8.40vw;
+	color: rgb(255, 255, 255);
+	background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
+	width: 1600px;
 
 }
 
 html {
-  margin: 0;
-  padding: 0;
+	margin: 0;
+	padding: 0;
 
 }
 
 h2 {
-  font-size: 56px;
-  transform: translateX(-5px);
-  font-family: var(--font-display);
-  margin-bottom: 0;
-  color: black;
+	font-size: 56px;
+	transform: translateX(-5px);
+	font-family: var(--font-display);
+	margin-bottom: 0;
+	color: black;
 }
 
 h3 {
-  font-size: 28px;
-  transform: translateX(-5px);
-  font-family: var(--font-display);
-  margin-bottom: 0;
-  color: black;
+	font-size: 28px;
+	transform: translateX(-5px);
+	font-family: var(--font-display);
+	margin-bottom: 0;
+	color: black;
 }
 
 
 p {
-  font-size: 24px;
-  width: 1600 px;
-  height: auto;
-  display: flex;
-  align-items: center;
-  margin: auto;
-  padding-top: 50px;
-  color: black;
+	font-size: 24px;
+	width: 1600 px;
+	height: auto;
+	display: flex;
+	align-items: center;
+	margin: auto;
+	padding-top: 50px;
+	color: black;
 
 
 
 }
 
 #IMGarticle {
-  position: relative;
-  color: white;
+	position: relative;
+	color: white;
 
 
 }
 
 #gradiant {
-  background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
-  width: 850px;
-  height: 100px;
-  position: relative;
-  margin-top: -100px;
-  border-radius: 20px;
+	background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
+	width: 850px;
+	height: 100px;
+	position: relative;
+	margin-top: -100px;
+	border-radius: 20px;
 }
 
 #articleIMG {
-  width: 850px;
-  height: auto;
-  display: flex;
-  align-items: none;
-  border-radius: 20px;
+	width: 850px;
+	height: auto;
+	display: flex;
+	align-items: none;
+	border-radius: 20px;
 
 }
 
 #fixedbutton {
-  width: 50px;
-  height: 50px;
-  border-radius: 9px;
-  box-shadow: 5px 5px 14px #bebebe,
-    -5px -5px 14px #ffffff;
+	width: 50px;
+	height: 50px;
+	border-radius: 9px;
+	box-shadow: 5px 5px 14px #bebebe,
+		-5px -5px 14px #ffffff;
 }
 
 .fix {
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  bottom: 20px;
-  right: 20px;
-  background-color: transparent;
-  font-family: sans-serif;
+	position: fixed;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 8px;
+	bottom: 20px;
+	right: 20px;
+	background-color: transparent;
+	font-family: sans-serif;
 
-  width: 50px;
-  height: 50px;
-  border-radius: 9px;
-  box-shadow: 5px 5px 14px #bebebe,
-    -5px -5px 14px #ffffff;
+	width: 50px;
+	height: 50px;
+	border-radius: 9px;
+	box-shadow: 5px 5px 14px #bebebe,
+		-5px -5px 14px #ffffff;
 
 }
 
 .favorite {
-  border: 0;
-  background-color: transparent;
+	border: 0;
+	background-color: transparent;
 }
 
 .favorite:hover {
-  border: 0;
-  background-color: transparent;
+	border: 0;
+	background-color: transparent;
 }
 </style>
