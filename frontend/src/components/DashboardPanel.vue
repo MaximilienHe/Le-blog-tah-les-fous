2b3f2395ed9371d239a08efea84fcf03a0f1e3ad
Eliot <bianchineliot@gmail.com>
2022-12-05 19:55:21 +0100

    création d'article DashboardPanel.vue

--- frontend/src/components/DashboardPanel.vue
+++ frontend/src/components/DashboardPanel.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="dashboard">
-        <img src="../assets/S22ultra.jpg" />
+        <div class="pushArticle">
+            <a href='/articleEditor'><button class = "PushButton">Écrire un article</button></a>
+        </div>
     </div>
+
+
 </template>
 
 <script>
@@ -11,5 +15,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.PushButton{
+    
+background-color: #555555;
+color: white;
+border-color: #555555;
+font-size: 20px;
+padding: 16px;
+}
+
+.pushArticle{
+margin: 10px 0px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+}
 
 </style>
