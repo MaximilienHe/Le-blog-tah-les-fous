87a65c97a92294a50735497a562e0d90311fbef3
Maximilien Herr <herrmaximilien@gmail.com>
2022-12-05 13:41:02 +0100

    CommentItem.vue component finished + beginning of AddComment.vue

--- Frontend/FrontEndWithVite/src/components/CommentItem.vue
+++ Frontend/FrontEndWithVite/src/components/CommentItem.vue
@@ -1,150 +1,53 @@
 <template>
-    <div class="comment">
-        
-    </div>
-
-
+  <div class="comment">
+    <p v-if="comment" class="pseudoU"> {{ comment.author}}</p>
+    <p v-if="comment" class="contenuComment"> {{ comment.content}}</p>
+  </div>
 </template>
-  
+
 <script>
 export default {
-    props: ['comment'],
-    // Properties returned from data() become reactive state
-    // and will be exposed on `this`.
-    data() {
-        return {};
+  props: ["comment"],
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+  data() {
+    return {};
+  },
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event listeners in templates.
+  methods: {
+    clicked() {
+      this.handleClick(this.post);
     },
-    // Methods are functions that mutate state and trigger updates.
-    // They can be bound as event listeners in templates.
-    methods: {
-        clicked() {
-            this.handleClick(this.post);
-        },
-    },
-    // Lifecycle hooks are called at different stages
-    // of a component's lifecycle.
-    // This function will be called when the component is mounted.
-    mounted() {
-    },
+  },
+  // Lifecycle hooks are called at different stages
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted.
+  mounted() {},
 };
 
-
+// function to iterate over the comments and return the comment item
 </script>
-  
+
 <style lang="scss" scoped>
-.tag {
-    --color-text: #fff;
-    --color-front: rgba(0, 0, 0, 0.231);
-    --color-back: rgba(0, 0, 0, 0.021);
-    position: relative;
-    display: inline-block;
-    border-radius: 6px;
-    clip-path: polygon(20px 0px, 100% 0px, 100% 100%, 0% 100%, 0% 20px);
-    background: var(--color-front);
-    padding: 10px 30px;
-    margin: 0 8px;
-    font-weight: 600;
-    font-size: 18px;
-    color: var(--color-text);
-    transition: clip-path 500ms;
+* {
+  font-family: "Rockwell";
+  font-size: 20px;
+}
+.comment {
+  width: 38vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 4vh;
+  padding: 0 1vw;
+  border-radius: 16px;
+  background: #e0e0e0;
+  box-shadow: 12px 12px 37px #929292, -12px -12px 37px #ffffff;
 }
 
-.tag:after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 20px;
-    height: 20px;
-    background: var(--color-back);
-    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.1);
-    border-radius: 0 0 6px 0;
-    transition: transform 500ms;
-}
-
-.tag:hover {
-    clip-path: polygon(0px 0px, 100% 0px, 100% 100%, 0% 100%, 0% 0px);
-}
-
-.tag:hover:after {
-    transform: translate(-100%, -100%);
-}
-
-.article {
-    display: block;
-    width: 100%;
-    max-width: 1280px;
-    margin: 0 auto;
-
-    .post {
-        margin: 0;
-        width: 100%;
-        display: flex;
-        background-color: #ffffff;
-        box-shadow: 3px 3px 8px 0px rgba(0, 0, 0, 0.2);
-        padding: 20px 20px;
-
-        .left {
-            margin: 0;
-            display: flex;
-            flex-direction: space-between;
-
-            .top-right {
-                padding-left: 20px;
-
-                .extrait {
-                    line-height: 25px;
-                }
-            }
-
-            .bottom-right {
-                display: flex;
-                justify-content: space-between;
-
-                .author {
-                    display: flex;
-                    justify-content: space-between;
-
-                    #AuthorName {
-                        padding-left: 5px;
-                    }
-                }
-            }
-        }
-
-        img {
-            width: 45%;
-            aspect-ratio: 16/9;
-            object-fit: contain;
-        }
-
-        h4 {
-            box-sizing: border-box;
-            height: 50px;
-            line-height: 50px;
-            margin: 0px;
-        }
-
-        &.col-2 {
-            grid-column: span 2;
-        }
-
-        &.row-2 {
-            grid-row: span 2;
-        }
-    }
-}
-
-button {
-    margin-top: 50px;
-    width: 100%;
-    background-color: #000000;
-    color: #ffffff;
-    padding: 15px 0;
-    font-size: 18px;
-    font-weight: 600;
-    border-radius: 5px;
-    cursor: pointer;
+.pseudoU {
+    padding-right: 3vw;
 }
 </style>
-  
--- Frontend/FrontEndWithVite/src/components/PostDetail.vue
+++ Frontend/FrontEndWithVite/src/components/PostDetail.vue
@@ -2,229 +2,256 @@
 import { usePiniaStore } from "../stores/postsStore";
 import Header from "../components/Header.vue";
 import CommentItem from "../components/CommentItem.vue";
+import AddComment from "../components/AddComment.vue";
 
 export default {
-	props: ["id", "postItem"],
-	components: {
-		Header,
-		CommentItem
-	},
-	// Properties returned fsrom data() become reactive state
-	// and will be exposed on `this`.
-	data() {
-		return {
-			post: undefined,
-			comments : []
-		};
-	},
-	// Methods are functions that mutate state and trigger updates.
-	// They can be bound as event listeners in templates.
-	methods: {},
-	// Lifecycle hooks are called at different stages
-	// of a component's lifecycle.
-	// This function will be called when the component is mounted.
-	mounted() {
-		const postStore = usePiniaStore();
-		this.post = postStore.getPost(this.$route.params.id);
-		console.log(this.post);
+  props: ["id", "postItem"],
+  components: {
+    Header,
+    CommentItem,
+	AddComment
+  },
+  // Properties returned fsrom data() become reactive state
+  // and will be exposed on `this`.
+  data() {
+    return {
+      post: undefined,
+      comments: [],
+    };
+  },
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event listeners in templates.
+  methods: {},
+  // Lifecycle hooks are called at different stages
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted.
+  mounted() {
+    const postStore = usePiniaStore();
+    this.post = postStore.getPost(this.$route.params.id);
+    console.log(this.post);
 
-		let URL = '/articles/' + this.post.id + '/comments';
-		fetch(URL)
-            .then((res) => res.json())
-            .then((res) => {
-                this.comments = res.data;
-            });
-	}
+    let URL = "/articles/" + this.post.id + "/comments";
+    fetch(URL)
+      .then((res) => res.json())
+      .then((res) => {
+        this.comments = res.data;
+      });
+  },
 };
 </script>
 
 <template>
+  <Header />
 
-	<Header />
+  <body>
+    <div class="banner">
+      <img v-if="post" :src="post.img" />
+    </div>
+    <div class="direction">
+      <h1 v-if="post">{{ post.title }}</h1>
+      <div id="infos">
+        <div v-if="post" class="author">{{ post.author }}</div>
+        <div v-if="post" class="date">{{ post.createdAt }}</div>
+      </div>
+    </div>
+    <div class="fix">
+      <button class="favorite">
+        <img src="../assets/bookmark.jpg" id="fixedbutton" />
+      </button>
+    </div>
 
-	<body>
-		<div class="banner">
-			<img v-if="post" :src="post.img" />
-		</div>
-		<div class="direction">
-			<h1 v-if="post">{{ post.title }}</h1>
-			<div id="infos">
-				<div v-if="post" class="author">{{ post.author }}</div>
-				<div v-if="post" class="date">{{ post.createdAt }}</div>
-			</div>
-		</div>
-		<div class="fix">
-			<button class="favorite"><img src="../assets/bookmark.jpg" id="fixedbutton"></button>
-		</div>
+    <div v-if="post" class="content">{{ post.content }}</div>
 
-		<div class="content"> {{ post.content }}</div>
-
-		<!-- <div id="IMGarticle">
+    <!-- <div id="IMGarticle">
         <img id="articleIMG" src="https://droidsoft.fr/wp-content/uploads/2022/11/Xiaomi-12T-Pro-miniature-scaled.jpg"
           data-v-cf937a3e />
         <div id="gradiant"></div>
       </div> -->
-		<div v-for="comment in comments" class="listComments">
-			<CommentItem class="item" :comment="comment" />
-		</div>
-	</body>
+    <section class="Commentaire">
+      <h3 class="CommentTitle">Commentaires</h3>
+	  <AddComment />
+      <div class="listComments">
+        <CommentItem />
+		<CommentItem />
+		<CommentItem />
+		<CommentItem />
+      </div>
+      <div v-for="comment in comments" class="listComments">
+        <CommentItem class="item" :comment="comment" />
+      </div>
+    </section>
+  </body>
 </template>
 
 <style scoped>
-.content {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: left;
-	padding-left: 20vh;
-	padding-right: 20vh;
-	color: white;
+.Commentaire {
+  padding: 0 15vh;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
+  padding-left: 20vh;
+  padding-right: 20vh;
+  color: white;
+}
 
 .banner {
-	position: relative;
-	padding-left: 7.5vw;
-
+  position: relative;
+  padding-left: 7.5vw;
 }
 
 .img {
-	width: 100%;
-	height: auto;
-	opacity: 0.6;
-	color: white;
-	border-radius: 20px;
+  width: 100%;
+  height: auto;
+  opacity: 0.6;
+  color: white;
+  border-radius: 20px;
 }
 
 #infos {
-	display: flex;
-	flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 
-
 h1 {
-	font-size: 78px;
-	margin-left: 50px;
-	margin-right: 50px;
-	color: white;
+  font-size: 78px;
+  margin-left: 50px;
+  margin-right: 50px;
+  color: white;
 }
 
 .author {
-	margin-left: 50px;
-	margin-right: 50px;
+  margin-left: 50px;
+  margin-right: 50px;
 }
 
 .date {
-	margin-left: 50px;
-	margin-right: 50px;
+  margin-left: 50px;
+  margin-right: 50px;
 }
 
 .direction {
-	position: absolute;
-	bottom: -12.7vh;
-	left: 8.40vw;
-	color: rgb(255, 255, 255);
-	background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
-	width: 1600px;
-
+  position: absolute;
+  bottom: -5vh;
+  left: 8.4vw;
+  color: rgb(255, 255, 255);
+  background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
+  width: 1600px;
 }
 
 html {
-	margin: 0;
-	padding: 0;
-
+  margin: 0;
+  padding: 0;
 }
 
 h2 {
-	font-size: 56px;
-	transform: translateX(-5px);
-	font-family: var(--font-display);
-	margin-bottom: 0;
-	color: black;
+  font-size: 56px;
+  transform: translateX(-5px);
+  font-family: var(--font-display);
+  margin-bottom: 0;
+  color: black;
 }
 
 h3 {
-	font-size: 28px;
-	transform: translateX(-5px);
-	font-family: var(--font-display);
-	margin-bottom: 0;
-	color: black;
+  font-size: 28px;
+  transform: translateX(-5px);
+  font-family: var(--font-display);
+  margin-bottom: 0;
+  color: black;
 }
 
-
 p {
-	font-size: 24px;
-	width: 1600 px;
-	height: auto;
-	display: flex;
-	align-items: center;
-	margin: auto;
-	padding-top: 50px;
-	color: black;
-
-
-
+  font-size: 24px;
+  width: 1600 px;
+  height: auto;
+  display: flex;
+  align-items: center;
+  margin: auto;
+  padding-top: 50px;
+  color: black;
 }
 
 #IMGarticle {
-	position: relative;
-	color: white;
-
-
+  position: relative;
+  color: white;
 }
 
 #gradiant {
-	background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
-	width: 850px;
-	height: 100px;
-	position: relative;
-	margin-top: -100px;
-	border-radius: 20px;
+  background: linear-gradient(to top, rgba(0, 0, 0, 0.85), transparent);
+  width: 850px;
+  height: 100px;
+  position: relative;
+  margin-top: -100px;
+  border-radius: 20px;
 }
 
 #articleIMG {
-	width: 850px;
-	height: auto;
-	display: flex;
-	align-items: none;
-	border-radius: 20px;
-
+  width: 850px;
+  height: auto;
+  display: flex;
+  align-items: none;
+  border-radius: 20px;
 }
 
 #fixedbutton {
-	width: 50px;
-	height: 50px;
-	border-radius: 9px;
-	box-shadow: 5px 5px 14px #bebebe,
-		-5px -5px 14px #ffffff;
+  width: 50px;
+  height: 50px;
+  border-radius: 9px;
+  box-shadow: 5px 5px 14px #bebebe, -5px -5px 14px #ffffff;
 }
 
 .fix {
-	position: fixed;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	gap: 8px;
-	bottom: 20px;
-	right: 20px;
-	background-color: transparent;
-	font-family: sans-serif;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  bottom: 20px;
+  right: 20px;
+  background-color: transparent;
+  font-family: sans-serif;
 
-	width: 50px;
-	height: 50px;
-	border-radius: 9px;
-	box-shadow: 5px 5px 14px #bebebe,
-		-5px -5px 14px #ffffff;
-
+  width: 50px;
+  height: 50px;
+  border-radius: 9px;
+  box-shadow: 5px 5px 14px #bebebe, -5px -5px 14px #ffffff;
 }
 
 .favorite {
-	border: 0;
-	background-color: transparent;
+  border: 0;
+  background-color: transparent;
 }
 
 .favorite:hover {
-	border: 0;
-	background-color: transparent;
+  border: 0;
+  background-color: transparent;
+}
+
+h2:after {
+  content: "";
+  display: block;
+  height: 8px;
+  background: gray;
+  max-width: 75px;
+  transform: skewX(-21deg) translateY(0px);
+  border-radius: 3px;
+}
+
+h3:after {
+  content: "";
+  display: block;
+  height: 8px;
+  background: gray;
+  max-width: 75px;
+  transform: skewX(-21deg) translateY(0px);
+  border-radius: 3px;
 }
 </style>
