8806607be01d5a20678ec276e1b66323e2f43d0c
Maximilien Herr <herrmaximilien@gmail.com>
2022-12-10 00:16:25 +0100

    Test de deploy

---
+++ frontend/src/pages/favorites.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header />
+    <ListPostsFavorites />
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import ListPostsFavorites from "../components/ListFavorites.vue";
+export default {
+  name: "app",
+  components: {
+    Header,
+    ListPostsFavorites,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+body {
+  background-color: #f3f3f3;
+  font-family: "Rockwell", serif;
+  h3 {
+    color: #171717;
+    font-size: 28px;
+    font-weight: 700;
+  }
+}
+</style>
