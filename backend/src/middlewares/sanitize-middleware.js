589fc5f444f81b0b11a3a2b34694b26beb28f4b5
FA <50623635+IrexUB@users.noreply.github.com>
2023-01-30 18:09:21 +0100

    Update sanitize-middleware.js

--- backend/src/middlewares/sanitize-middleware.js
+++ backend/src/middlewares/sanitize-middleware.js
@@ -1,7 +1,7 @@
 const xss = require('xss');
 
 function sanitize() {
-    console.log(xss("<script></script><h1><h1><img src=\"pute\"></>"))
+    console.log(xss("<script></script><h1><h1><img src=\"test\"></>"))
 }
 
 sanitize()
